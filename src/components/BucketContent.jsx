2b2c14f793411fc8d1a8967f99985114f8976b06
Gwali <gwalisam37@gmail.com>
2023-05-01 15:56:09 +0000

    code plan

---
+++ src/components/BucketContent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function BucketContent() {
+  //useffect to get user info ,  buckets
+  return (
+    <div className="container">
+      <p>navbar component</p>
+      <p>bucket navbar</p>
+      <p>songs</p>
+      <p>footer</p>
+    </div>
+  );
+}
+
+export default BucketContent;
