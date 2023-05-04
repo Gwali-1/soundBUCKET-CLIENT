bd68c4507482a73dceb28b25b0e2a54b74f1bd4d
Gwali <gwalisam37@gmail.com>
2023-05-04 16:07:18 +0000

    added a search component to search for songs

--- src/components/BucketActions.jsx
+++ src/components/BucketActions.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Box from "@mui/material/Box";
+
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import RestoreIcon from "@mui/icons-material/Restore";
@@ -8,11 +8,56 @@ import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+//search song
 
 function BucketActions() {
   const [value, setValue] = React.useState(0);
+  const [songs, setSongs] = React.useState(null);
   return (
     <>
+      {value === 1 && (
+        <Box
+          sx={{
+            width: 500,
+            maxWidth: "100%",
+            margin: "0px auto 0px auto",
+          }}
+        >
+          <TextField
+            fullWidth
+            label="Search Song"
+            id="fullWidth"
+            onChange={(e) => {
+              fetch(
+                `http://127.0.0.1:8000/sportify/find_song?track=${e.target.value}`,
+                { method: "POST" }
+              )
+                .then((response) => response.json())
+                .then((result) => {
+                  setSongs(result);
+                })
+                .catch((err) => {
+                  setSongs(null);
+                });
+            }}
+          />
+
+          <div className="p-2 border">
+            {songs &&
+              songs.map((song) => {
+                return (
+                  <li>
+                    {song.title}-{song.artist_name}
+                  </li>
+                );
+              })}
+          </div>
+        </Box>
+      )}
+
       <Paper
         sx={{
           position: "fixed",
